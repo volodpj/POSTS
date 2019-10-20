73cc5a9317aeff2de2703a89e240e019e74deb9c
volodpj <volod.af@gmail.com>
2019-10-20 09:36:21 +0300

    Updates

---
+++ precache-manifest.dc9ebe9c7a40e0f78383d96dffda41b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be8da704fff2197ea9d516f1089fcac",
+    "url": "/POSTS/index.html"
+  },
+  {
+    "revision": "d2f4089804ed947bb5cc",
+    "url": "/POSTS/static/css/main.053179e5.chunk.css"
+  },
+  {
+    "revision": "21b33f7903f7adfd2252",
+    "url": "/POSTS/static/js/2.793e6e84.chunk.js"
+  },
+  {
+    "revision": "d2f4089804ed947bb5cc",
+    "url": "/POSTS/static/js/main.46c670c0.chunk.js"
+  },
+  {
+    "revision": "b6c07f4ba343ea1d9494",
+    "url": "/POSTS/static/js/runtime~main.dc43f2e5.js"
+  }
+]);
